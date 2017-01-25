e1181ce2d9f6eba32b2d05303d60c32c87f0ea56
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-01-25 16:02:16 +0200

    complete

--- functional-lists.js
+++ functional-lists.js
@@ -24,15 +24,14 @@ EmptyList.prototype.push = function(x) {
 	return listNode; 
 };
 
-EmptyList.prototype.remove = function(x) { /* implement this */ };
-EmptyList.prototype.append = function(xs) { /* implement this */ };
+EmptyList.prototype.remove = function(x) {};
+EmptyList.prototype.append = function(xs) {};
 
 
 function ListNode(value, next) { 
 	this.value = value;
 	this.next = next;
 }
-
 
 
 ListNode.prototype = new List();
@@ -53,11 +52,11 @@ ListNode.prototype.toString = function() {
 };
 
 ListNode.prototype.head = function() { 
-	return this;
+	return this.value;
 };
 
 ListNode.prototype.tail = function() { 
-	return this.next;
+	return this.next; 
 };
 ListNode.prototype.length = function() {
 	let length = 0;
@@ -69,14 +68,44 @@ ListNode.prototype.length = function() {
 	return length;
 };
 
-ListNode.prototype.push = function(x) { 
+ListNode.prototype.push = function(x) {
 	let listNode = new ListNode(x, this);
 	return listNode;
 };
 
-ListNode.prototype.remove = function(x) { /* implement this */ };
+ListNode.prototype.remove = function(x) {
+	if (x == this.head()){
+		return this.next;	
+	}
+	let length = this.length();
+	let list = this;
+	let prevNode;
+
+	prevNode = list;
+
+	if(list.value === x) {
+		list = list.next;
+		length--;
+	}
+list.r = true;
+
+	while (length > 1) {
+		list = list.next;
+		list.r = true;
+		if(list.value === x) {
+			prevNode.next = list.next;
+		} else {
+			prevNode = list;	
+		}
+
+		length--;
+	}
+
+	return this;
+};
+
+
 ListNode.prototype.append = function(xs) {
-	console.log(xs.toString())
 	let length = this.length();
 	let list = this;
 	let copyThis, headCopy;
@@ -89,7 +118,6 @@ ListNode.prototype.append = function(xs) {
 	while (length >= 1) {
 		length--;
 		list = list.next;
-		console.log(list.value);
 		copyThis = new ListNode(list.value, undefined);
 		prevNode.next = copyThis;
 		prevNode = copyThis;
@@ -97,33 +125,3 @@ ListNode.prototype.append = function(xs) {
 	prevNode.next = xs;
 	return headCopy;
 };
-
-
-
-var mt, l1, l2, l3, l4;
-
-mt = new EmptyList();
-l1 = mt.push('c').push('b').push('a');
-l2 = mt.push('c');
-l3 = l1.append(l1);
-
-
-
-
-
-console.log(l1.toString());
-console.log(l3.toString());
-/*l3 = l1.remove('b');
-l4 = l2.remove('b');
-
-
-console.log(mt.isEmpty(), "Empty List is empty");
-console.log( !l1.isEmpty(), "Non-empty list is not empty");
-console.log(mt.toString() === "()", "()");
-console.log(l3.toString() === "(a c)", "(a c)");
-console.log(mt.length() === 0, "Empty list has length zero");
-console.log(l1.length() === 3, "(a b c) length 3");
-
-console.log(l2.tail().tail().tail() === l1, "(a b c a b c) shares");
-console.log(l2 !== l1, "(a b c a b c) doesn't share too much");
-console.log(l3.tail() === l1.tail().tail(), "(a b c) remove b shares c");*/
