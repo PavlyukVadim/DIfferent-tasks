510f5003cc43e356856bd06742f277a91b7008fd
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-01-25 14:05:14 +0200

    add append

--- functional-lists.js
+++ functional-lists.js
@@ -37,12 +37,28 @@ function ListNode(value, next) {
 
 ListNode.prototype = new List();
 ListNode.prototype.constructor = ListNode;
-ListNode.prototype.isEmpty = function() { /* implement this */ };
+ListNode.prototype.isEmpty = function() { 
+	if (!this.length()) return true;
+	return false;
+};
 
-ListNode.prototype.toString = function() { /* implement this */ };
+ListNode.prototype.toString = function() { 
+	let list = this,
+		values = [];
+	while (list instanceof ListNode) {
+		values.push(list.value);
+		list = list.next;
+	}
+	return "(" + values.join(" ") + ")";
+};
 
-ListNode.prototype.head = function() { /* implement this */ };
-ListNode.prototype.tail = function() { /* implement this */  };
+ListNode.prototype.head = function() { 
+	return this;
+};
+
+ListNode.prototype.tail = function() { 
+	return this.next;
+};
 ListNode.prototype.length = function() {
 	let length = 0;
 	let list = this;
@@ -59,7 +75,28 @@ ListNode.prototype.push = function(x) {
 };
 
 ListNode.prototype.remove = function(x) { /* implement this */ };
-ListNode.prototype.append = function(xs) { /* implement this */ };
+ListNode.prototype.append = function(xs) {
+	console.log(xs.toString())
+	let length = this.length();
+	let list = this;
+	let copyThis, headCopy;
+	let prevNode;
+
+	copyThis = new ListNode(list.value, undefined);
+	headCopy = copyThis;	 
+	prevNode = copyThis;
+	length--;
+	while (length >= 1) {
+		length--;
+		list = list.next;
+		console.log(list.value);
+		copyThis = new ListNode(list.value, undefined);
+		prevNode.next = copyThis;
+		prevNode = copyThis;
+	}
+	prevNode.next = xs;
+	return headCopy;
+};
 
 
 
@@ -67,11 +104,16 @@ var mt, l1, l2, l3, l4;
 
 mt = new EmptyList();
 l1 = mt.push('c').push('b').push('a');
+l2 = mt.push('c');
+l3 = l1.append(l1);
 
-console.log(l1.length());
-/*
-l2 = l1.append(l1);
-l3 = l1.remove('b');
+
+
+
+
+console.log(l1.toString());
+console.log(l3.toString());
+/*l3 = l1.remove('b');
 l4 = l2.remove('b');
 
 
