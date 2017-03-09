5fa765bd69efa64c2d3b28ace2b9e7860321c254
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-09 18:23:53 +0200

    add toString

--- codewars/4 kyu/funnel.js
+++ codewars/4 kyu/funnel.js
@@ -2,10 +2,18 @@
 
 function Funnel() {
 	this.data = [Array(1), Array(2), Array(3), Array(4), Array(5)];
+	this.data.forEach((level) => {
+		let i = 0;
+		for (; i < level.length; i++) {
+			level[i] = ' ';	
+		} 
+	});
 }
 
 Funnel.prototype.toString = function() {
-
+	//
+	return  this.data.map(level => ' '.repeat(5 - level.length) + '\\' + level.join(' ') + '/' + ' '.repeat(5 - level.length)).reverse().join('\n');
+	//return '\\' + ' ' + '/'
 };
 
 Funnel.prototype.fill = function() {
