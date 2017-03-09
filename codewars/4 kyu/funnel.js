b94645759f2fd043b209cdabe87c85c8ff304e51
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-09 15:33:50 +0200

    initial funnel

---
+++ codewars/4 kyu/funnel.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function Funnel() {
+	this.data = [Array(1), Array(2), Array(3), Array(4), Array(5)];
+}
+
+Funnel.prototype.toString = function() {
+
+};
+
+Funnel.prototype.fill = function() {
+
+};
+
+Funnel.prototype.drip = function() {
+
+};
+
+let funnel = new Funnel();
+console.log(funnel.toString());
