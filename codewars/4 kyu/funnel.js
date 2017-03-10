bee2608fa2ac6e44e8167b45a7cc972422d1515a
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-10 10:45:00 +0200

    fill

--- codewars/4 kyu/funnel.js
+++ codewars/4 kyu/funnel.js
@@ -8,16 +8,30 @@ function Funnel() {
 			level[i] = ' ';	
 		} 
 	});
+	this.capacity = 0;
 }
 
 Funnel.prototype.toString = function() {
-	//
 	return  this.data.map(level => ' '.repeat(5 - level.length) + '\\' + level.join(' ') + '/' + ' '.repeat(5 - level.length)).reverse().join('\n');
-	//return '\\' + ' ' + '/'
 };
 
-Funnel.prototype.fill = function() {
-
+Funnel.prototype.fill = function(value) {
+	if (this.capacity >= 14) return;
+	let i = 0;
+	let filled = false;
+	for (; i < 5; i++) {
+		let level = this.data[i];
+		let j = 0;
+		for (; j < level.length; j++) {
+			if(level[j] == ' ') {
+				level[j] = value;
+				filled = true;
+				this.capacity += 1;
+				break;
+			};	
+		}
+		if (filled) break;
+	}
 };
 
 Funnel.prototype.drip = function() {
@@ -25,4 +39,15 @@ Funnel.prototype.drip = function() {
 };
 
 let funnel = new Funnel();
+funnel.fill(1);
+funnel.fill(2);
+funnel.fill(3);
+funnel.fill(4);
+funnel.fill(5);
+funnel.fill(6);
+funnel.fill(7);
+funnel.fill(8);
+funnel.fill(9);
+
+
 console.log(funnel.toString());
