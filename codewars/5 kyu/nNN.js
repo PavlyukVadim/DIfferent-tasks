90a0374d63a91246c43887d0bd8c7ae16a4bb4e0
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-13 22:00:25 +0200

    in 37 symbols

--- codewars/5 kyu/nNN.js
+++ codewars/5 kyu/nNN.js
@@ -2,5 +2,5 @@
   return (!(index % 2)) ? ('' + element + element == a[index + 1]) ? 1 : 0 : 1;
 });*/
 
-nNN = a => (a+='').match(/(\d),\1{2}/g)==a
-console.log(nNN([1,11,2,22,4,44]));
+nNN = a => (a+='').match(/(.),\1{2}/g)==a
+console.log(nNN([1,11,2,22,4,04]));
