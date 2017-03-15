e3d48668b49f81e62f7af2b3f51e7bacd00368eb
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-15 21:47:48 +0200

    countSmileys

--- codewars/6 kyu/countSmileys.js
+++ codewars/6 kyu/countSmileys.js
@@ -3,9 +3,8 @@
 //return the total number of smiling faces in the array
 function countSmileys(arr) {
   return arr.reduce((sum, face) => {
-  	if(/[:;][-~]?[)D]/.test(face)) {
-  		console.log(face);
-  	} return sum + 1;
+  	if(/[:;][-~]?[)D]/.test(face)) 
+  	  return sum + 1;
   	return sum;
   }, 0);
 }
