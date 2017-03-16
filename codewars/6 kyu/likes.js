bc15433c5afce98ae9528f6cbcf5ffe49bbd850d
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-16 23:21:43 +0200

    like

--- codewars/6 kyu/likes.js
+++ codewars/6 kyu/likes.js
@@ -3,9 +3,9 @@
 function likes(names) {
   if (!names.length) return 'no one likes this';
   else if (names.length === 1) return `${names[0]} likes this`;
-  else if (names.length === 2) return `${names[0]} and ${names[1]} likes this`;
-  else if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} likes this`;
-  else return `${names[0]}, ${names[1]} and ${names.length - 2} others likes this`;
+  else if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  else if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  else return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
 
 console.log(likes([]));
