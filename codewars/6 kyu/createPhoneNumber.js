ab1368611308fde1698adc9a859d11c47cd720c0
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-17 22:11:53 +0200

    createPhoneNumber

---
+++ codewars/6 kyu/createPhoneNumber.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function createPhoneNumber(numbers) {
+  numbers = numbers.join('');
+  return `(${numbers.slice(0, 3)}) ${numbers.slice(3, 6)}-${numbers.slice(6)}`
+}
+
+//return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
