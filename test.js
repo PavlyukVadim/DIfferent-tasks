83f97fd1cc9ddf38ad1ffc542e60fe4d06abc6af
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-20 08:21:49 +0300

    test

--- test.js
+++ test.js
@@ -1,2 +1,21 @@
-a="The eagle has landed";
-console.log(a.length);
+"use strict";
+
+class Animal {
+  constructor(name = 'no-name') {
+    this.name = name;
+  }
+
+  getName() {
+  	return this.name;
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+  	return `Dog ${this.name} is barking`;
+  }
+}
+
+const dog = new Dog('Aban');
+console.log(dog.getName() === 'Aban');
+console.log(dog.bark() === 'Dog Aban is barking');
