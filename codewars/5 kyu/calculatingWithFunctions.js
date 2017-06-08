c3850183fbafeb7f79df6d4aa99d4351b6cbe0aa
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-06-08 23:29:45 +0300

    calculatingWithFunctions

--- codewars/5 kyu/calculatingWithFunctions.js
+++ codewars/5 kyu/calculatingWithFunctions.js
@@ -1,29 +1,9 @@
-"use strict";
-
-const zero = (...arg) => arg[0] ? arg[0](zero): zero; 
-zero.valueOf = () => 0; 
-const one = (...arg) => arg[0] ? arg[0](one): one; 
-one.valueOf = () => 1; 
-const two = (...arg) => arg[0] ? arg[0](two): two; 
-two.valueOf = () => 2; 
-const three = (...arg) => arg[0] ? arg[0](three): three; 
-three.valueOf = () => 3; 
-const four = (...arg) => arg[0] ? arg[0](four): four; 
-four.valueOf = () => 4; 
-const five = (...arg) => arg[0] ? arg[0](five): five; 
-five.valueOf = () => 5; 
-const six = (...arg) => arg[0] ? arg[0](six): six; 
-six.valueOf = () => 6; 
-const seven = (...arg) => arg[0] ? arg[0](seven): seven; 
-seven.valueOf = () => 7; 
-const eight = (...arg) => arg[0] ? arg[0](eight): eight; 
-eight.valueOf = () => 8;
-const nine = (...arg) => arg[0] ? arg[0](nine): nine; 
-nine.valueOf = () => 9;
-
-const plus = (...arg) => (a) => a + arg[0];
-const minus = (...arg) => (a) => a - arg[0];
-const times = (...arg) => (a) => a * arg[0];
-const dividedBy = (...arg) => (a) => a / arg[0];
-
-console.log(seven(times(five()))); // must return 35)
+const
+  id = x => x,
+  number = x => (f = id) => f(x),
+  [zero, one, two, three, four, five, six, seven, eight, nine] =
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(number),
+  plus = x => y => y + x,
+  minus = x => y => y - x,
+  times = x => y => y * x,
+  dividedBy = x => y => y / x;
