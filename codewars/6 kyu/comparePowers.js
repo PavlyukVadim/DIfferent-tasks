c3ef37bd0923b80aad5e33295df754e3a9260d07
Pavlyuk Vadim <front1@insfero.com>
2017-07-27 16:21:25 +0300

    comparePowers

---
+++ codewars/6 kyu/comparePowers.js
@@ -0,0 +1,13 @@
+const comparePowers = (n1, n2) => {
+  if (n1[0] === n2[1] && n1[1] === n2[0] || n1[0] === n2[0] && n1[0] === 1) {
+    return 0;
+  } else if(n1[1] === n2[1]) {
+    return ((n2[0] > n1[0]) ? 1 : ((n2[0] < n1[0]) ? -1 : 0));
+  } else if(n1[0] === n2[0]) {
+    return ((n2[1] > n1[1]) ? 1 : ((n2[1] < n1[1]) ? -1 : 0));
+  } else {
+    const p1 = Math.log(n1[0]) * n1[1];
+    const p2 = Math.log(n2[0]) * n2[1];
+    return ((p2 > p1) ? 1 : (( p2 < p1 ) ? -1 : 0));
+  }
+}
